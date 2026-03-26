5ba4538a5513ed76870384ea1afc388632eb5e37
Rizki Adrian <rizki.adrian@surya-am.com>
2026-03-26 22:46:30 +0700

    chore(login): add secure cookie function

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "subject-case": [0, "never"], // Membebaskan huruf besar/kecil di subject
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // Fitur baru
+        "fix", // Perbaikan bug
+        "docs", // Dokumentasi
+        "style", // Styling (bukan perubahan logic)
+        "refactor", // Refactoring kode
+        "perf", // Peningkatan performa
+        "test", // Menambah/memperbaiki test
+        "build", // Build system atau dependencies
+        "ci", // Konfigurasi CI/CD
+        "chore", // Maintenance rutin
+        "revert", // Membatalkan commit sebelumnya
+      ],
+    ],
+    "subject-case": [0], // 0 artinya rule ini dimatikan (sesuai mau kamu)
   },
 };
